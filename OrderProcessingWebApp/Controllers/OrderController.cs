4da761630b99d6276ff074eeb9cb65aaab160031
mohsi <mohsi@MOHSIN-PC>
2025-03-19 00:10:00 +0500

    Add project files.

---
+++ OrderProcessingWebApp/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderProcessingWebApp.Models;
+
+namespace OrderProcessingWebApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProcessOrder(OrderModel model)
+        {
+            if (model.OrderAmount >= 100 && model.CustomerType == "Loyal")
+            {
+                model.Discount = model.OrderAmount * 0.10m;
+            }
+            else
+            {
+                model.Discount = 0;
+            }
+
+            model.FinalTotal = model.OrderAmount - model.Discount;
+
+            return View("OrderResult", model);
+        }
+
+    }
+}
---
+++ OrderProcessingWebApp/Models/OrderModel.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderProcessingWebApp.Models
+{
+    public class OrderModel
+    {
+        public decimal OrderAmount { get; set; }
+        public string CustomerType { get; set; } = "New"; // Default value
+        public decimal Discount { get; set; }
+        public decimal FinalTotal { get; set; }
+    }
+}
